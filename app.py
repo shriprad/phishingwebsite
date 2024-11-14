4e7645992599e68d859dd2a29bcf7b49230302a9
shriprad <shriprad.c@gmail.com>
2024-11-14 10:30:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def analyze():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 100000)), debug=True)
