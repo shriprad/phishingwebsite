38601c86ff079b8e0fc411b58df7822aa358bcb2
shriprad <shriprad.c@gmail.com>
2024-11-14 10:22:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def analyze():
     return jsonify(analysis)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
