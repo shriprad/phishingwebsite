7683c2bd7b730bc5b161dbea73680e43cd939b45
shriprad <shriprad.c@gmail.com>
2024-11-20 07:46:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def check_aws():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
